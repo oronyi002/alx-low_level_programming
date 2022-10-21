b44fb5d171f770cad05d9b557fc84a3d95e2dc69
oronyi002 <leshaujune@gmail.com>
2022-10-21 05:39:53 -0700

    header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_number(void);
+void print_most_number(void);
+void more_number(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _putchar(char c);
+
+#endif
